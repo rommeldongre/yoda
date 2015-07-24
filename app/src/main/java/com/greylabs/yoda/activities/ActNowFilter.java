1c219d0795e83f0bed40fbcc4d408e775497e109
Rohit Jagtap <rohitjagtap1991@gmail.com>
2015-07-24 13:46:20 +0530

    ActNowFilter design done

--- app/src/main/java/com/greylabs/yoda/activities/ActNowFilter.java
+++ app/src/main/java/com/greylabs/yoda/activities/ActNowFilter.java
@@ -1,8 +1,9 @@
 package com.greylabs.yoda.activities;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -15,14 +16,16 @@ import com.greylabs.yoda.scheduler.AlarmScheduler;
 import com.greylabs.yoda.utils.Constants;
 import com.greylabs.yoda.utils.Logger;
 
-public class ActNowFilter extends ActionBarActivity implements View.OnClickListener {
+public class ActNowFilter extends Activity implements View.OnClickListener {
 
     TextView tvGoalName, tvStepName, tvTime;
     LinearLayout llDidIt, llDoingIt, llMissedIt;
-    Toolbar toolbar;
-    Button btnCloseActivity;
+    CardView cvEmptyView, cvNotEmptyView;
+//    Toolbar toolbar;
+    Button btnCloseNotEmptyView, btnCloseEmptyView;
     private PendingStep pendingStep;
     private Goal goal;
+    String caller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,20 +35,24 @@ public class ActNowFilter extends ActionBarActivity implements View.OnClickListe
     }
 
     private void initialize() {
-        toolbar = (Toolbar) findViewById(R.id.toolBarActNowFilter);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle(getResources().getString(R.string.titleActAddNewStep));
+//        toolbar = (Toolbar) findViewById(R.id.toolBarActNowFilter);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setTitle(getResources().getString(R.string.titleActAddNewStep));
 
-        btnCloseActivity = (Button) findViewById(R.id.btnCloseActNowFilter);
+        btnCloseNotEmptyView = (Button) findViewById(R.id.btnCloseNotEmptyViewActNowFilter);
+        btnCloseEmptyView = (Button) findViewById(R.id.btnCloseEmptyViewActNowFilter);
         tvGoalName = (TextView) findViewById(R.id.tvGoalNameActNowFilter);
         tvStepName = (TextView) findViewById(R.id.tvStepNameActNowFilter);
         tvTime = (TextView) findViewById(R.id.tvTimeActNowFilter);
         llDidIt = (LinearLayout) findViewById(R.id.llDidActNowFilter);
         llDoingIt = (LinearLayout) findViewById(R.id.llDoingItActNowFilter);
         llMissedIt = (LinearLayout) findViewById(R.id.llMissedActNowFilter);
+        cvNotEmptyView = (CardView) findViewById(R.id.cvNotEmptyViewActNowFilter);
+        cvEmptyView = (CardView) findViewById(R.id.cvEmptyViewActNowFilter);
 
-        btnCloseActivity.setOnClickListener(this);
+        btnCloseNotEmptyView.setOnClickListener(this);
+        btnCloseEmptyView.setOnClickListener(this);
         llDidIt.setOnClickListener(this);
         llDoingIt.setOnClickListener(this);
         llMissedIt.setOnClickListener(this);
@@ -54,12 +61,31 @@ public class ActNowFilter extends ActionBarActivity implements View.OnClickListe
     }
 
     private void getCurrentStepScheduledFromLocal() {
-        AlarmScheduler alarmScheduler=(AlarmScheduler)getIntent().getSerializableExtra(Constants.ALARM_SCHEDULER);
-        pendingStep=new PendingStep(this).get(alarmScheduler.getStepId());
-        goal=new Goal(this).get(pendingStep.getGoalId());
-        tvGoalName.setText(goal.getNickName());
-        tvStepName.setText(pendingStep.getNickName());
-        tvTime.setText(String.valueOf(alarmScheduler.getStartTime()));
+        caller = getIntent().getStringExtra(Constants.CALLER);
+        if(caller.equals(Constants.ALARM_SERVICE)){
+            AlarmScheduler alarmScheduler=(AlarmScheduler)getIntent().getSerializableExtra(Constants.ALARM_SCHEDULER);
+            pendingStep=new PendingStep(this).get(alarmScheduler.getStepId());
+            goal=new Goal(this).get(pendingStep.getGoalId());
+            tvGoalName.setText(goal.getNickName());
+            tvStepName.setText(pendingStep.getNickName());
+            tvTime.setText(String.valueOf(alarmScheduler.getStartTime()));
+        }else if(caller.equals(Constants.ACT_HOME)){
+            // get the current step from local
+            pendingStep = new PendingStep(this);
+
+            checkForEmptyViewVisibility();
+        }
+    }
+
+    private void checkForEmptyViewVisibility() {
+        if (pendingStep == null) {
+            cvEmptyView.setVisibility(View.VISIBLE);
+            cvNotEmptyView.setVisibility(View.GONE);
+        }
+        else {
+            cvEmptyView.setVisibility(View.GONE);
+            cvNotEmptyView.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
@@ -80,7 +106,11 @@ public class ActNowFilter extends ActionBarActivity implements View.OnClickListe
                 finish();
                 break;
 
-            case R.id.btnCloseActNowFilter :
+            case R.id.btnCloseNotEmptyViewActNowFilter:
+                finish();
+                break;
+
+            case R.id.btnCloseEmptyViewActNowFilter:
                 finish();
                 break;
         }
