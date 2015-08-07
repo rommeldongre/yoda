5067028b79334bdd3d2f6de471d0b53810f5959a
jaybhayvijay <jaybhayvijay@gmail.com>
2015-08-07 14:26:09 +0530

    Color Code methods added in the Goal,PendingStep model

--- app/src/main/java/com/greylabs/yoda/activities/ActHome.java
+++ app/src/main/java/com/greylabs/yoda/activities/ActHome.java
@@ -49,7 +49,6 @@ public class ActHome extends Activity implements View.OnClickListener, FloatingA
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Day d=new Day(this);
         initialize();
     }
 
